acd51f120917d5fc54e86b6000534136f49b0123
shehara1218 <IT26102121@my.sliit.lk>
2026-06-16 18:23:18 +0530

    IT26102121LAB2Q3.java

---
+++ Q3/IT26102121LAB2Q3.java
@@ -0,0 +1,14 @@
+public class IT26102121LAB2Q3{
+	public static void main(String[]args){
+		
+		double sideA=3;
+		double sideB=4;
+		
+		double hypotenuse = Math.sqrt((sideA*sideA)+(sideB*sideB));
+		
+		System.out.println("sideA=" +sideA);
+		System.out.println("sideB=" +sideB);
+		System.out.println("Hypotenuse=" +hypotenuse);
+		
+	}
+}
