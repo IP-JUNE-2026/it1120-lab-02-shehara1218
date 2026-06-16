2b42443731158f6c8e6448fb92e64f4d4cf7863a
shehara1218 <IT26102121@my.sliit.lk>
2026-06-16 16:08:07 +0530

    Lab2Q1.java

---
+++ Q1/LAB2Q1.java
@@ -0,0 +1,13 @@
+public class LAB2Q1 {//Starting the class
+
+  public static void main(String[]args){//starting of main method}
+
+  
+        double perimeter = 100;
+        double length = perimeter / (2*(1+3.0/4));
+        double width = (3.0/4)*length;
+         
+         System.out.println("Length of the fence: "+length);
+         System.out.println("width of the fence: "+width);
+  }
+  }  //end of main method        
